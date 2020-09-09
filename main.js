3ac43f5d81d0081afc7859b501231af9a68a5a4f
Ricki DiCristoforo <rickidicristoforo@Rickis-MacBook-Air.local>
2020-09-09 11:15:15 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// Script to open and close sidebar
+function sb_open() {
+    document.getElementById("mySidebar").style.display = "block";
+}
+    
+function sb_close() {
+    document.getElementById("mySidebar").style.display = "none";
+}
