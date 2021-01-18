e6bc6b97b2147af2c54b2ef3e9128e4b8cadbe75
Ricki DiCristoforo <rdicrist@gmail.com>
2021-01-18 10:43:23 -0700

    updated file paths to php versions

---
+++ partials/_aboutme.php
@@ -0,0 +1,13 @@
+<div class="aboutme">  
+    <h3>About Me, Ricki Dee</h3><br>
+    <img src="style/images/catrick.jpg" alt="Me" class="w3-image" style="display:block;margin:auto">
+    <div class="w3-padding-32">
+        <h5><i>I hate your "hubby" and "kiddos" so much!</i></h5>
+        <div class="width"><p>I had a dream. A dream to look up a recipe on the internet and not be assaulted with 
+            long blog posts about the day to day life of the author. I do not care what your "hubby"s 
+            favorite recipe is. I do not care what season or childhood memory it reminds you of. I 
+            do not care about your personal journey of making this recipe.
+            I just want to see the ingredients and the steps.
+        </p></div>
+    </div>
+</div>
---
+++ partials/_footer.php
@@ -0,0 +1,5 @@
+<footer id="footer">
+    <div class="foote">
+        <p>&copy; Ricki DiCristoforo 2021</p>
+    </div>
+</footer>
